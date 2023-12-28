82b950d59d9ef6580603c674b63ed371689b9442
Sonava <sonava@code.edu.az>
2023-12-28 12:15:40 +0400

    Scroll added

--- Scroll Animation/script.js
+++ Scroll Animation/script.js
@@ -1,3 +1,23 @@
 "use strict";
 
 const animationItems = document.querySelectorAll(".animationItem");
+
+const scrollAnimation = () => {
+    animationItems.forEach((animationItem) => {
+
+        const triggerPoint = window.innerHeight * 0.8;
+        const distanceFromTop = animationItem.getBoundingClientRect().top;
+      
+        animationItem.style.transition = "all 200ms linear"
+      
+        if (distanceFromTop < triggerPoint) {
+          animationItem.style.opacity = 1;
+          animationItem.style.transform = "translateY(0)";
+        } else {
+          animationItem.style.opacity = 0;
+          animationItem.style.transform = "translateY(50px)";
+        }
+      });
+}
+
+window.addEventListener("scroll", scrollAnimation)
