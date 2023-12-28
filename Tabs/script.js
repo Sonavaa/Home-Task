65d29ef28979d4301893e237e7727b541d0f0d60
Sonava <sonava@code.edu.az>
2023-12-28 11:04:17 +0400

    Edited Tabs

--- Tabs/script.js
+++ Tabs/script.js
@@ -4,14 +4,15 @@ const tabsContainer = document.querySelector(".tabs-container");
 const tabs = document.querySelectorAll(".tab");
 const tabContents = document.querySelectorAll(".tab__content");
 
-tabs.forEach((item, index, arr) => {
-    item.addEventListener("click", () => {
-        const tab = +item.dataset.tab;
+tabs.forEach((tab_btn) => {
+    tab_btn.addEventListener("click", () => {
+        const tab = +tab_btn.dataset.tab;
 
         for (let i = 0; i < tabContents.length; i++) {
             tabContents[i].classList.add("hidden");
-            tabxs[i].classList.remove("-translate-y-1/4")
+            tabs[i].classList.remove("-translate-y-1/4")
         }
+        
         tabContents[tab].classList.remove("hidden");
         tabs[tab].classList.add("-translate-y-1/4")
     });
